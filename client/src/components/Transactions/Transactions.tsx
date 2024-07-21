ebaa7c568420ebcb1bc5ab21f96cfea600384bc2
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-07-21 18:20:27 +0300

    table row edit

--- client/src/components/Transactions/Transactions.tsx
+++ client/src/components/Transactions/Transactions.tsx
@@ -1,15 +1,18 @@
-import { useRef } from "react";
+import { FunctionComponent, useCallback, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 import {
-  GridReadyEvent,
-  GridApi,
   ColDef,
   ValueFormatterParams,
+  RowValueChangedEvent,
 } from "ag-grid-community";
+
 import { useGetAllTransactionsQuery } from "src/app/services/transactionApi";
 import { AG_GRID_LOCALE_RU } from "src/utils/locale.ru";
+import type { CustomCellRendererProps } from "@ag-grid-community/react";
+import { Button } from "@nextui-org/react";
+import { MdDeleteOutline } from "react-icons/md";
 
 const dateFormatter = (params: ValueFormatterParams): string => {
   return new Date(params.value).toLocaleDateString("ru-ru", {
@@ -20,49 +23,18 @@ const dateFormatter = (params: ValueFormatterParams): string => {
   });
 };
 
-const columnDefs: ColDef[] = [
-  {
-    headerName: "Name",
-    field: "name",
-    minWidth: 50,
-    flex: 1,
-    filter: true,
-  },
-  {
-    headerName: "Type",
-    field: "type",
-    minWidth: 50,
-    flex: 1,
-  },
-  {
-    headerName: "Date",
-    field: "date",
-    minWidth: 50,
-    flex: 1,
-    filter: "agDateColumnFilter",
-    valueFormatter: dateFormatter,
-  },
-  {
-    headerName: "Amount",
-    field: "amount",
-    minWidth: 50,
-    flex: 1,
-    filter: "agNumberColumnFilter",
-    valueFormatter: (params: ValueFormatterParams) => {
-      return "₽ " + params.value.toLocaleString();
-    },
-  },
-  {
-    headerName: "Category",
-    field: "category",
-    minWidth: 50,
-    flex: 1,
-  },
-];
+const ActionsCellRenderer: FunctionComponent<CustomCellRendererProps> = ({
+  node,
+}) => {
+  const onRemoveClick = () => {
+    console.log(node.data.id);
+  };
 
-type AgGridApi = {
-  grid?: GridApi;
-  column?: GridApi;
+  return (
+    <button onClick={onRemoveClick}>
+      <MdDeleteOutline />
+    </button>
+  );
 };
 
 const gridOptions = {
@@ -71,28 +43,95 @@ const gridOptions = {
 
 export const TransactionsList = () => {
   const { data } = useGetAllTransactionsQuery();
-  const apiRef = useRef<AgGridApi>({
-    grid: undefined,
-    column: undefined,
+  const categories: string[] = [];
+  data?.categories.forEach((item) => {
+    categories.push(item.category);
   });
-  const onGridReady = (params: GridReadyEvent) => {
-    apiRef.current.grid = params.api;
-    apiRef.current.column = params.api;
-  };
+  console.log(categories);
+  const columnDefs: ColDef[] = [
+    {
+      headerName: "Name",
+      field: "name",
+      minWidth: 50,
+      flex: 1,
+      filter: true,
+      editable: true,
+      valueSetter: (params) => {
+        console.log(params.newValue);
+        return true;
+      },
+    },
+    {
+      headerName: "Type",
+      field: "type",
+      minWidth: 50,
+      flex: 1,
+      editable: true,
+      cellEditor: "agSelectCellEditor",
+      cellEditorParams: {
+        values: ["income", "expense"],
+      },
+      valueSetter: (params) => {
+        console.log("changed Type");
+        return true;
+      },
+    },
+    {
+      headerName: "Date",
+      field: "date",
+      minWidth: 50,
+      flex: 1,
+      filter: "agDateColumnFilter",
+      valueFormatter: dateFormatter,
+    },
+    {
+      headerName: "Amount",
+      field: "amount",
+      minWidth: 50,
+      flex: 1,
+      filter: "agNumberColumnFilter",
+      valueFormatter: (params: ValueFormatterParams) => {
+        return "₽ " + params.value.toLocaleString();
+      },
+    },
+    {
+      headerName: "Category",
+      field: "category",
+      minWidth: 50,
+      flex: 1,
+      editable: true,
+      cellEditor: "agSelectCellEditor",
+      cellEditorParams: {
+        values: categories,
+      },
+      valueSetter: (params) => {
+        console.log("changed Category");
+        return true;
+      },
+    },
+    {
+      field: "",
+      cellRenderer: ActionsCellRenderer,
+      width: 50,
+      resizable: false,
+    },
+  ];
+
+  const onRowValueChanged = useCallback((event: RowValueChangedEvent) => {
+    const data = event.data;
+    console.log(data);
+  }, []);
 
   return (
     <div className="w-full">
-      <div
-        style={{ height: "100%", width: "100%" }}
-        className="ag-theme-quartz "
-      >
+      <div className="ag-theme-quartz w-full h-full">
         <AgGridReact
           rowSelection="multiple"
-          suppressRowClickSelection
           columnDefs={columnDefs}
-          onGridReady={onGridReady}
           rowData={data?.transactions}
           gridOptions={gridOptions}
+          onRowValueChanged={onRowValueChanged}
+          editType={"fullRow"}
         />
       </div>
     </div>
