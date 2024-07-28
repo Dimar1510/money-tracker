01de6d9595c37229627fffaab6f5714aa6bac53e
dimar1150 <158440383+Dimar1510@users.noreply.github.com>
2024-07-28 12:32:57 +0300

    for deploy

--- client/src/pages/404/NotFound.tsx
+++ client/src/pages/404/NotFound.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>404</div>;
+  return (
+    <div className="h-svh flex flex-col gap-10 justify-center items-center">
+      <h1 className="text-7xl">404</h1>
+      <h2>Страница не найдена</h2>
+      <Link className="text-xl underline underline-offset-4" to={"/"}>
+        Перейти на главную
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
